ea1cade7e09fd933b62eb73c14f368e38adf6df5
tmt-thao <vika.tran.minh@gmail.com>
2024-12-03 19:07:22 +0100

    Layout Children

--- client/src/pages/AuthorPost.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AuthorPost = () => {
-  return (
-    <div>AuthorPost</div>
-  )
-}
-
-export default AuthorPost
--- client/src/pages/CategoryPost.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CategoryPost = () => {
-  return (
-    <div>CategoryPost</div>
-  )
-}
-
-export default CategoryPost

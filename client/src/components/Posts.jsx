7c3529f0d335ea0834b8498fa217bc3b96a084db
tmt-thao <vika.tran.minh@gmail.com>
2024-12-04 17:41:35 +0100

    Posts logic

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,11 +1,33 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import PostItem from './PostItem'
-import { DUMMY_POSTS } from '../data'
+import Loader from '../components/Loader'
 
 const Posts = () => {
-    const [posts, setPosts] = useState(DUMMY_POSTS)
+    const [posts, setPosts] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setIsLoading(true)
+
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/posts`)
+                setPosts(response?.data)
+            } catch (err) {
+                console.log(err);
+            }
+
+            setIsLoading(false)
+        }
+
+        fetchPosts()
+    }, [])
+
+    if (isLoading) {
+        return <Loader />
+    }
 
     return (
         <section className="posts">
@@ -20,7 +42,7 @@ const Posts = () => {
                                     thumbnail,
                                     category,
                                     title,
-                                    desc,
+                                    description,
                                     authorID
                                 }) => <PostItem
                                         key={id}
@@ -28,7 +50,7 @@ const Posts = () => {
                                         thumbnail={thumbnail}
                                         category={category}
                                         title={title}
-                                        description={desc}
+                                        description={description}
                                         authorID={authorID} />
                             )
                         }

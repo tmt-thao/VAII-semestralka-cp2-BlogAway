94de58d0c06c44d34ed91d058745304f76292bb1
tmt-thao <vika.tran.minh@gmail.com>
2024-12-04 20:27:04 +0100

    AuthorPosts, PostAuthor logic

--- client/src/pages/AuthorPosts.jsx
+++ client/src/pages/AuthorPosts.jsx
@@ -1,41 +1,69 @@
-import React, { useState } from 'react'
-import { DUMMY_POSTS } from '../data'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+
 import PostItem from '../components/PostItem'
+import Loader from '../components/Loader'
 
 const AuthorPosts = () => {
+  const [posts, setPosts] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
-  const [posts, setPosts] = useState(DUMMY_POSTS)
+  const {id} = useParams()
 
-  return (
-    <section className="posts">
-      {
-        posts.length > 0
-          ?
-          <div className="container posts__container">
-            {
-              posts.map(
-                ({
-                  id,
-                  thumbnail,
-                  category,
-                  title,
-                  desc,
-                  authorID
-                }) => <PostItem
-                    key={id}
-                    postID={id}
-                    thumbnail={thumbnail}
-                    category={category}
-                    title={title}
-                    description={desc}
-                    authorID={authorID} />
-              )
-            }
-          </div>
-          : <h2 className='center'>No posts found</h2>
+  useEffect(() => {
+      const fetchPosts = async () => {
+          setIsLoading(true)
+
+          try {
+              const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/posts/users/${id}`)
+              setPosts(response?.data)
+          } catch (err) {
+              console.log(err);
+          }
+
+          setIsLoading(false)
       }
 
-    </section>
+      fetchPosts()
+  }, [])
+
+  if (isLoading) {
+      return <Loader />
+  }
+
+  return (
+      <section className="posts">
+          {
+              posts.length > 0
+                  ?
+                  <div className="container posts__container">
+                      {
+                          posts.map(
+                              ({
+                                  _id: id,
+                                  thumbnail,
+                                  category,
+                                  title,
+                                  description,
+                                  creator,
+                                  createdAt
+                              }) => <PostItem
+                                      key={id}
+                                      postID={id}
+                                      thumbnail={thumbnail}
+                                      category={category}
+                                      title={title}
+                                      description={description}
+                                      authorID={creator}
+                                      createdAt={createdAt} />
+                          )
+                      }
+                  </div>
+                  : <h2 className='center'>No posts found</h2>
+          }
+
+      </section>
   )
 }
 

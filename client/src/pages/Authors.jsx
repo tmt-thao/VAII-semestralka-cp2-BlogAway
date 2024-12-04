19380476030fe65e2e8ffcb1a9dbc490cb996bdc
tmt-thao <vika.tran.minh@gmail.com>
2024-12-04 20:47:48 +0100

    Authors logic

--- client/src/pages/Authors.jsx
+++ client/src/pages/Authors.jsx
@@ -1,23 +1,34 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-
-import Avatar1 from '../images/avatar1.jpg'
-import Avatar2 from '../images/avatar2.jpg'
-import Avatar3 from '../images/avatar3.jpg'
-import Avatar4 from '../images/avatar4.jpg'
-import Avatar5 from '../images/avatar5.jpg'
-
-const authorData = [
-  { id: 1, avatar: Avatar1, name: 'Percy Jackson', posts: 3 },
-  { id: 2, avatar: Avatar2, name: 'Annabeth Chase', posts: 5 },
-  { id: 3, avatar: Avatar3, name: 'Grover Underwood', posts: 0 },
-  { id: 4, avatar: Avatar4, name: 'Thalia Grace', posts: 2 },
-  { id: 5, avatar: Avatar5, name: 'Leo Valdez', posts: 1 },
-]
+import axios from 'axios'
+import Loader from '../components/Loader'
 
 const Authors = () => {
 
-  const [authors, setAuthors] = useState(authorData);
+  const [authors, setAuthors] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    const getAuthors = async () => {
+      setIsLoading(true)
+
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/users`)
+        setAuthors(response.data)
+      } catch (error) {
+        console.log(error);
+        
+      }
+
+      setIsLoading(false)
+    }
+
+    getAuthors()
+  }, [])
+
+  if (isLoading) {
+    return <Loader />
+  }
 
   return (
     <section className="authors">
@@ -26,10 +37,10 @@ const Authors = () => {
           ?
           <div className="container authors__container">
             {
-              authors.map(({ id, avatar, name, posts }) => {
+              authors.map(({ _id: id, avatar, name, posts }) => {
                 return <Link className='author' key={id} to={`/posts/users/${id}`}>
                   <div className='author__avatar'>
-                    <img src={avatar} alt={`${name}`} />
+                    <img src={`${process.env.REACT_APP_ASSETS_URL}/uploads/${avatar}`} alt={`${name}`} />
                   </div>
                   <div className="author__info">
                     <h4>{name}</h4>

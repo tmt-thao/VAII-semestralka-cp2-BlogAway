beec7aa675664ab713dd7424f058c3bfcb6de594
tmt-thao <vika.tran.minh@gmail.com>
2024-12-04 11:41:05 +0100

    Login User

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const HttpError = require("../models/errorModel")
 const User = require("../models/userModel")
@@ -31,6 +32,7 @@ const registerUser = async (req, res, next) => {
         const salt = await bcrypt.genSalt(10)
         const hashedPass = await bcrypt.hash(password, salt)
         const newUser = await User.create({name, email: newEmail, password: hashedPass})
+        
         res.status(201).json(`New user ${newUser.email} registered.`)
     } catch (error) {
         return next(new HttpError("User registration failed.", 422))
@@ -41,7 +43,30 @@ const registerUser = async (req, res, next) => {
 // POST request: api/users/login
 // UNPROTECTED
 const loginUser = async (req, res, next) => {
-    res.json("Login User")
+    try {
+        const {email, password} = req.body
+        if (!email || !password) {
+            return next(new HttpError("Fill in all fields.", 422))
+        }
+
+        const newEmail = email.toLowerCase();
+        const user = await User.findOne({email: newEmail})
+        if (!user) {
+            return next(new HttpError("Invalid credentials.", 422))
+        }
+
+        const comparePass = await bcrypt.compare(password, user.password)
+        if (!comparePass) {
+            return next(new HttpError("Invalid credentials.", 422))
+        }
+
+        const {_id: id, name} = user;
+        const token = jwt.sign({id, name}, process.env.JWT_SECRET, {expiresIn: "1d"})
+
+        res.status(200).json({token, id, name})
+    } catch (error) {
+        return next(new HttpError("Login failed. Please check your credentials.", 422))
+    }
 }
 
 // USER PROFILE =====================================================

7826bb15ae5c95bbf966ce6baa4e1b127ac2d133
tmt-thao <vika.tran.minh@gmail.com>
2024-12-04 17:16:32 +0100

    Header logic

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../images/logo.png'
 import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 
+import { UserContext} from '../context/userContext'
+
 const Header = () => {
 
   const [isNavShowing, setIsNavShowing] = useState(window.innerWidth > 800 ? true : false)
+  const {currentUser} = useContext(UserContext)
 
   const closeNavHandler = () => {
     if (window.innerWidth < 800) {
@@ -23,11 +26,16 @@ const Header = () => {
           <img src={Logo} alt="Navbar Logo" />
         </Link>
 
-        {isNavShowing && <ul className="nav__menu">
+        {currentUser?.id && isNavShowing && <ul className="nav__menu">
           <li><Link to="/profile/sdfsdf" onClick={closeNavHandler}>Percy Jackson</Link></li>
           <li><Link to="/create" onClick={closeNavHandler}>Create Post</Link></li>
           <li><Link to="/authors" onClick={closeNavHandler}>Authors</Link></li>
           <li><Link to="/logout" onClick={closeNavHandler}>Logout</Link></li>
+        </ul>}
+        
+        {!currentUser?.id && isNavShowing && <ul className="nav__menu">
+          <li><Link to="/authors" onClick={closeNavHandler}>Authors</Link></li>
+          <li><Link to="/login" onClick={closeNavHandler}>Login</Link></li>
         </ul>}
 
         <button className="nav__toggle-btn" onClick={() => setIsNavShowing(!isNavShowing)}>

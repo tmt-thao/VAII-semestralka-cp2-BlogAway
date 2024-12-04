54769908aa195c9af12e60f55bb127994050f453
tmt-thao <vika.tran.minh@gmail.com>
2024-12-04 21:08:16 +0100

    Create Post logic

--- client/src/components/PostItem.jsx
+++ client/src/components/PostItem.jsx
@@ -17,7 +17,7 @@ const PostItem = ({ postID, category, title, description, authorID, thumbnail, c
                 <Link to={`/posts/${postID}`}>
                     <h3>{postTitle}</h3>
                 </Link>
-                <p>{shortDescription}</p>
+                <p dangerouslySetInnerHTML={{__html: shortDescription}} />
                 <div className="post__footer">
                     <PostAuthor authorID={authorID} createdAt={createdAt} />
                     <Link to={`/posts/categories/${category}`} className="btn category">{category}</Link>
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -3,6 +3,7 @@ import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 import {UserContext} from '../context/userContext'
 import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
 
 const CreatePost = () => {
 
@@ -10,6 +11,7 @@ const CreatePost = () => {
   const [category, setCategory] = useState('Uncategorized')
   const [description, setDescription] = useState('')
   const [thumbnail, setThumbnail] = useState('')
+  const [error, setError] = useState('')
 
   const navigate = useNavigate()
   const {currentUser} = useContext(UserContext)
@@ -44,16 +46,33 @@ const CreatePost = () => {
     'Uncategorized', 'Weather'
   ]
 
+  const createPost = async (e) => {
+    e.preventDefault()
+
+    const postData = new FormData()
+    postData.set('title', title)
+    postData.set('category', category)
+    postData.set('description', description)
+    postData.set('thumbnail', thumbnail)
+
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/posts`, postData, {withCredentials: true, headers: {Authorization: `Bearer ${token}`}})
+      if (response.status == 201) {
+        return navigate('/')
+      }
+    } catch (error) {
+      setError(error.response.data.message)
+    }
+  }
+
   return (
     <section className="create-post">
       <div className="container">
         <h2>Create Post</h2>
 
-        <p className="form__error-message">
-          This is an error message
-        </p>
+        {error && <p className="form__error-message">{error}</p>}
 
-        <form className="form create-post__form">
+        <form className="form create-post__form" onSubmit={createPost}>
           <input type="text" placeholder='Title' value={title} onChange={e => setTitle(e.target.value)} autoFocus />
           
           <select name="category" value={category} onChange={e => setCategory(e.target.value)}>

b1c81f49373bd186a1bc8d5e59fa7bd16c71af57
tmt-thao <vika.tran.minh@gmail.com>
2024-12-04 10:20:38 +0100

    MongoDB and Main Routes Setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const cors = require('cors')
-const {connect} = require('mongoose')
+const { connect } = require('mongoose')
 require('dotenv').config()
 
-const app = express()
+const userRoutes = require('./routes/userRoutes')
+const postRoutes = require('./routes/postRoutes')
 
-app.listen(5000, () => console.log("Server running on port 5000"))
+const app = express()
+app.use(express.json({extended: true}))
+app.use(express.urlencoded({extended: true}))
+app.use(cors({credentials: true, origin: "http://localhost:3000"}))
+
+app.use('/api/users', userRoutes)
+app.use('/api/posts', postRoutes)
+
+connect(process.env.MONGO_URI)
+    .then(app.listen(5000, () => console.log(`Server running on port ${process.env.PORT}`)))
+    .catch(error => { console.log(error) })

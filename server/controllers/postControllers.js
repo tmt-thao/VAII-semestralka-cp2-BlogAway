59f33ba4f68cd749dea64397d2bc68da5557aa2c
tmt-thao <vika.tran.minh@gmail.com>
2024-12-04 14:46:23 +0100

    Create Post

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -1,10 +1,49 @@
+const Post = require('../models/postModel')
+const User = require('../models/userModel')
+const path = require('path')
+const fs = require('fs')
+const {v4: uuid} = require('uuid')
+const HttpError = require('../models/errorModel')
 
 
 // ==== CREATE A POST ===========================================
 // POST: api/posts
 // PROTECTED
 const createPost = async (req, res, next) => {
-    res.json("Create Post")
+    try {
+        let {title, category, description} = req.body;
+        if (!title || !category || !description || !req.files) {
+            return next(new HttpError("Fill in all fields and choose thumbnail.", 422))
+        }
+
+        const {thumbnail} = req.files
+        if (thumbnail.size > 2000000) {
+            return next(new HttpError("Thumbnail too big. File should be less than 2MB.", 422))
+        }
+
+        let fileName = thumbnail.name
+        let splittedFilename = fileName.split('.')
+        let newFilename = splittedFilename[0] + uuid() + '.' + splittedFilename[splittedFilename.length - 1]
+        thumbnail.mv(path.join(__dirname, '..', '/uploads', newFilename), async (err) => {
+            if (err) {
+                return next(new HttpError(err))
+            }
+
+            const newPost = await Post.create({title, category, description, thumbnail: newFilename, creator: req.user.id})
+            if (!newPost) {
+                return next(new HttpError("Post couldn't be created.", 422))
+            }
+
+            // find user and increase post count by 1
+            const currentUser = await User.findById(req.user.id);
+            const userPostCount = currentUser.posts + 1
+            await User.findByIdAndUpdate(req.user.id, {posts: userPostCount})
+
+            res.status(201).json(newPost)
+        })
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 // ==== GET ALL POSTS ===========================================

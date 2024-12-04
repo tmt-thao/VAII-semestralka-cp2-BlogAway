986d73e38024cef6cc9e7c6e79e5fedce9ba26a1
tmt-thao <vika.tran.minh@gmail.com>
2024-12-04 14:12:08 +0100

    Add Auth Middlware to Post Routes

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,16 +1,17 @@
-const {Router} = require('express')
+const { Router } = require('express')
 
-const { createPost, getPost, getPosts, getCatPosts, 
+const { createPost, getPost, getPosts, getCatPosts,
     getUserPosts, editPost, deletePost } = require('../controllers/postControllers')
+const authMiddleware = require('../middleware/authMiddleware')
 
 const router = Router()
 
-router.post('/', createPost)
+router.post('/', authMiddleware, createPost)
 router.get('/', getPosts)
 router.get('/:id', getPost)
 router.get('/categories/:category', getCatPosts)
 router.get('/users/:id', getUserPosts)
-router.patch('/:id', editPost)
-router.delete('/:id', deletePost)
+router.patch('/:id', authMiddleware, editPost)
+router.delete('/:id', authMiddleware, deletePost)
 
 module.exports = router

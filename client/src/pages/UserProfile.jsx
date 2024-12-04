586ba79265bb9955a5af96e03450df2ecb9804f4
tmt-thao <vika.tran.minh@gmail.com>
2024-12-04 22:40:57 +0100

    UserProfile logic

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,19 +1,22 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import Avatar from '../images/avatar15.jpg'
 import { FaEdit } from "react-icons/fa";
 import { FaCheck } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom'
 import { UserContext } from '../context/userContext'
+import axios from 'axios'
 
 const UserProfile = () => {
 
-  const [avatar, setAvatar] = useState(Avatar);
+  const [avatar, setAvatar] = useState('');
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [currentPassword, setCurrentPassword] = useState('')
   const [newPassword, setNewPassword] = useState('')
   const [confirmNewPassword, setConfirmNewPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const [isAvatarTouched, setIsAvatarTouched] = useState(false)
 
   const navigate = useNavigate()
   const { currentUser } = useContext(UserContext)
@@ -24,7 +27,63 @@ const UserProfile = () => {
     if (!token) {
       navigate('/login')
     }
-  })
+  }, [])
+
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/users/${currentUser.id}`,
+        { withCredentials: true, headers: { Authorization: `Bearer ${token}` } }
+      )
+
+      const { name, email, avatar } = response.data
+      setName(name)
+      setEmail(email)
+      setAvatar(avatar)
+    }
+
+    getUser()
+  }, [])
+
+  const changeAvatarHandler = async () => {
+    setIsAvatarTouched(false)
+    try {
+      const postData = new FormData()
+      postData.set('avatar', avatar)
+      const response = await axios.post(
+        `${process.env.REACT_APP_BASE_URL}/users/change-avatar`, postData,
+        { withCredentials: true, headers: { Authorization: `Bearer ${token}` } }
+      )
+      setAvatar(response?.data.avatar)
+    } catch (error) {
+      console.log(error);
+
+    }
+  }
+
+  const updateUserDetails = async (e) => {
+    e.preventDefault()
+
+    try {
+      const userData = new FormData()
+      userData.set('name', name)
+      userData.set('email', email)
+      userData.set('currentPassword', currentPassword)
+      userData.set('newPassword', newPassword)
+      userData.set('confirmNewPassword', confirmNewPassword)
+
+      const response = await axios.patch(
+        `${process.env.REACT_APP_BASE_URL}/users/edit-user`,
+        userData, { withCredentials: true, headers: { Authorization: `Bearer ${token}` } }
+      )
+
+      if (response.status == 200) {
+        // log user out
+        navigate('/logout')
+      }
+    } catch (error) {
+      setError(error.response.data.message)
+    }
+  }
 
   return (
     <section className="profile">
@@ -34,23 +93,23 @@ const UserProfile = () => {
         <div className="profile__details">
           <div className="avatar__wrapper">
             <div className="profile__avatar">
-              <img src={avatar} alt="" />
+              <img src={`${process.env.REACT_APP_ASSETS_URL}/uploads/${avatar}`} alt="" />
             </div>
-            
+
             {/* Form to update avatar */}
             <form className="avatar__form">
               <input type="file" name='avatar' id='avatar' onChange={e => setAvatar(e.target.files[0])} accept='png, jpg, jpeg' />
-              <label htmlFor="avatar"><FaEdit/></label>
+              <label htmlFor="avatar" onClick={() => setIsAvatarTouched(true)}><FaEdit /></label>
             </form>
 
-            <button className='profile__avatar-btn'><FaCheck/></button>
+            {isAvatarTouched && <button className='profile__avatar-btn' onClick={changeAvatarHandler}><FaCheck /></button>}
           </div>
 
-          <h1>Percy Jackson</h1>
+          <h1>{currentUser.name}</h1>
 
           {/* Form to update user details */}
-          <form className="form profile__form">
-            <p className="form__error-message">This is an error message</p>
+          <form className="form profile__form" onSubmit={updateUserDetails}>
+            {error && <p className="form__error-message">{error}</p>}
             <input type="text" placeholder='Full Name' value={name} onChange={e => setName(e.target.value)} />
             <input type="email" placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} />
             <input type="password" placeholder='Current password' value={currentPassword} onChange={e => setCurrentPassword(e.target.value)} />

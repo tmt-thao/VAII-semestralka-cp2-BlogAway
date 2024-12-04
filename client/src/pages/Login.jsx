ed48397496851c2d490cc9e1bac7209b16deffec
tmt-thao <vika.tran.minh@gmail.com>
2024-12-04 17:06:08 +0100

    Login logic

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+import { UserContext } from '../context/userContext.js'
 
 const Login = () => {
 
@@ -8,10 +11,31 @@ const Login = () => {
     password: ''
   })
 
+  const [error, setError] = useState("")
+  const navigate = useNavigate()
+
+  const { setCurrentUser } = useContext(UserContext)
+
   const changeInputHandler = (e) => {
     setUserData(prevState => {
       return { ...prevState, [e.target.name]: e.target.value };
     })
+  }
+
+  const loginUser = async (e) => {
+    e.preventDefault()
+    setError('')
+
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/users/login`, userData)
+      const user = await response.data
+
+      setCurrentUser(user)
+
+      navigate('/')
+    } catch (error) {
+      setError(error.response.data.message)
+    }
   }
 
   return (
@@ -19,10 +43,8 @@ const Login = () => {
       <div className="container">
         <h2>Sign In</h2>
 
-        <form className="form login__form">
-          <p className="form__error-message">
-            This is an error message
-          </p>
+        <form className="form login__form" onSubmit={loginUser}>
+          {error && <p className="form__error-message">{error}</p>}
 
           <input type="text" placeholder='Email' name='email' value={userData.email} onChange={changeInputHandler} autoFocus />
           <input type="password" placeholder='Password' name='password' value={userData.password} onChange={changeInputHandler} />
